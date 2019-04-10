586290ed9d6067f35a2d0e9b1b5d257d676e76bb
michaelsweeney <itsN0T4me!>
2019-04-10 11:02:53 -0400

    heroku deploy test

--- app.py
+++ app.py
@@ -1,8 +1,199 @@
+import glob as gb
+import pandas as pd
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
 import dash
+from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
+import parse
+import webbrowser
+
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 app.config.suppress_callback_exceptions = True
 
 
+
+# todo allow manual rates (and handle 'inactive', fix and check conversions, stabilize viewports/divs. longer-term: consider sliding left sidenav
+config = {'showLink': False,
+          'displayModeBar': False}
+
+app.title = "NYC 80x50 energy + performance estimator"
+
+app.layout = dbc.Container([
+    dbc.Navbar([
+        dbc.Row(dbc.Col(html.H4("AKF Energy + Analytics NYC Carbon Fine Calculator", id='title'), width=12)),
+    ]),
+
+dbc.Row(dbc.Col(id='body', children=[dbc.Row(children=[
+        dbc.Col(id = 'sidenav',children=[
+            html.Div("General Info"),
+            dbc.InputGroup(
+                [dbc.InputGroupAddon("Bldg Area", addon_type="prepend"),
+                 dbc.Input(id='area_input', value='135000')]),
+
+            html.Div(className = 'input-group-text bldg_type_header', children=['Building Type']),
+            dcc.Dropdown(className = 'form-control',
+                         id='bldg_type',
+                         options=[
+                             {'label': "A, Assembly", 'value': 'A'},
+                             {'label': "E, Education", 'value': 'E'},
+                             {'label': "H, High Hazard", 'value': 'H'},
+                             {'label': "U, Utility & Misc.", 'value': 'U'},
+                             {'label': "B, Business", 'value': 'B'},
+                             {'label': "I, Institutional", 'value': 'I'},
+                             {'label': "M, Mercantile", 'value': 'M'},
+                             {'label': "F, Factory & Industrial", 'value': 'F'},
+                             {'label': "S, Storage", 'value': 'S'},
+                             {'label': "R, Residential", 'value': 'R'}
+                         ],
+                         value='R',
+                         placeholder='Building Type',
+                         clearable=False),
+            html.Br(),
+
+            html.Div(children=["Annual Consumption"]),
+            dbc.InputGroup(
+                [dbc.InputGroupAddon("Elec (kWh)", addon_type="prepend"),
+                 dbc.Input(id='ann_kwh_input', value='1897217')]),
+            dbc.InputGroup(
+                [dbc.InputGroupAddon("Gas (Therms)", addon_type="prepend"),
+                 dbc.Input(id='ann_gas_input', value=32103)]),
+
+            dbc.InputGroup(
+                [dbc.InputGroupAddon("Steam (MMBtu)", addon_type="prepend"), dbc.Input(id='ann_steam_input')]),
+            html.Br(),
+
+            html.Div("Annual Cost"),
+            dbc.InputGroup(
+                [dbc.InputGroupAddon("Elec ($)",
+                                     addon_type="prepend"),
+                 dbc.Input(id='ann_kwh_cost_input')]),
+            dbc.InputGroup(
+                [dbc.InputGroupAddon("Gas ($)",
+                                     addon_type="prepend"),
+                 dbc.Input(id='ann_gas_cost_input')]),
+
+            dbc.InputGroup(
+                [dbc.InputGroupAddon("Steam ($)",
+                                     addon_type="prepend"),
+                 dbc.Input(id='ann_steam_cost_input')]),
+
+            dbc.Checklist(id='cost_toggle', options=[
+                {'label': 'Use Default Rates', 'value': 'Y'}
+            ],
+                          values=['Y']
+                          ),
+            html.Br(),
+            dbc.Button('Submit', id='submit_btn')
+
+        ], width=2),
+
+        dbc.Col(width=2, children=[
+            dbc.Col([dcc.Graph(id='bullet_bar', config=config)
+                                            ], width=12), ]),
+
+        dbc.Col([
+
+            dbc.Row(id='figrow1', children=[
+                dbc.Col([dcc.Graph(id='pie_summaries', config=config)
+            ], width=12),]),
+
+            dbc.Row(id='figrow2', children=[
+
+                dbc.Col([dcc.Graph(id='cost_bar', config=config)
+
+                         ], width=12),
+            ])
+        ], width=8),
+
+    ]),], width=12)),
+
+
+
+dcc.Store(id='split_df_stores')
+], fluid=True)
+
+
+
+
+# @app.callback(
+# Output("summary_donut_eui", "figure"),
+# Input('cost_toggle', 'values'))
+# def cost_inactive(cost_toggle, value):
+#     print (value)
+
+@app.callback(
+[
+    Output("pie_summaries", "figure"),
+    Output("bullet_bar", "figure"),
+    Output("cost_bar", "figure"),
+],
+[
+    Input("submit_btn", "n_clicks"),
+    Input('bldg_type', 'value'),
+],
+[
+        State('area_input', 'value'),
+        State('ann_kwh_input', 'value'),
+        State('ann_gas_input', 'value'),
+        State('ann_steam_input', 'value'),
+        State('ann_kwh_cost_input', 'value'),
+        State('ann_gas_cost_input', 'value'),
+        State('ann_steam_cost_input', 'value'),
+        State('cost_toggle', 'values'),
+ ])
+
+def make_frame(n_clicks,
+               bldg_type,
+               area_input,
+               ann_kwh_input,
+               ann_gas_input,
+               ann_steam_input,
+               ann_kwh_cost_input,
+               ann_gas_cost_input,
+               ann_steam_cost_input,
+               cost_toggle):
+
+    input_cons_dict = {
+        'Elec': ann_kwh_input,
+        'Gas': ann_gas_input,
+        'Steam': ann_steam_input,
+    }
+
+    if cost_toggle == ["Y"]:
+        bldg = parse.Bldg(area_input,
+                          bldg_type,
+                          input_cons_dict).to_frame()
+
+    else:
+        input_rate_dict = {
+            'Elec': ann_kwh_cost_input,  # $ / kWh
+            'Gas': ann_gas_cost_input,  # $ / Therm
+            'Steam': ann_steam_cost_input,  # $ / MMBtu
+        }
+        bldg = parse.Bldg(area_input,
+                          bldg_type,
+                          input_cons_dict,
+                          input_rate_dict).to_frame()
+
+    cost = bldg[(bldg['Table'] == 'Cost') & (bldg['Value'] != 0)]
+    carbon = bldg[(bldg['Table'] == 'Carbon') & (bldg['Value'] != 0)]
+    eui = bldg[(bldg['Table'] == 'EUI') & (bldg['Value'] != 0)]
+    co2limit = bldg[bldg['Table'] == 'CO2 Limit']
+    fine = bldg[bldg['Table'] == 'Annual Fine']
+    pie_charts = parse.make_pie_subplots(cost, carbon, eui)
+    carbon_bullet = parse.make_carbon_bullet(carbon, co2limit)
+    cost_bar = parse.make_cost_bar(fine, cost)
+    return pie_charts, carbon_bullet, cost_bar
+
+
+if __name__ == "__main__":
+    port = 8881
+    # open in chrome #todo why does this open each time
+    # chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    # url = 'http://127.0.0.1:{0}'.format(port)
+    # webbrowser.get(chrome_path).open(url)
+    app.run_server(debug=True, port=port, dev_tools_hot_reload=True)
--- parse.py
+++ parse.py
@@ -7,20 +7,56 @@ import numpy as np
 import bunch
 
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly import tools
 import plotly.offline as py
+
 import plotly.graph_objs as go
 
-
-
-
+limits = pd.read_csv('nyc_carbon_limits.csv')
+limit_dict = limits
+limit_dict.index = limits['DOB Occupancy groups']
+limit_dict = limits[['2022-2023', '2024-2029', '2030-2049', '2050']]
+limit_dict = limit_dict.T.to_dict()
 
 piedims = 350
 piecolors = ['rgb(56, 151, 170)', 'rgb(83, 224, 120)', 'rgb(242, 224, 109)']
 
+# static dictionaries/lookups
+carbon_dict = {
+    'Elec': 0.000288962,  # tCO2 / kWh
+    'Gas': 0.005311,  # tCO2 / Therm
+    'Steam': 0.0064,  # tCO2/MMBtu (from 66.4 kg/MMBtu)
+}
+kbtu_dict = {  # convert to kbtu
+    'Elec': 3.412,
+    'Gas': 100,
+    'Steam': 1000,
+}
+
+input_std_dict_rates = {
+    'Elec': 0.18866,  # $ / kWh
+    'Gas': 0.997,  # $ / Therm
+    'Steam': 2.36,  # $ / MMBtu
+}
+
+
+def try_float(num):
+    '''returns float, else returns 0'''
+    try:
+        return float(num)
+    except:
+        return 0
+
+
+fine_per_tCO2 = 268
 
 
 class Bldg:
-    def __init__(self, area, bldg_type, cons_dict, rate_dict, carbon_dict, limit_dict, fineval, energy_dict):
+    def __init__(self, area, bldg_type, cons_dict, rate_dict=input_std_dict_rates):
+
+        area = try_float(area)
+        cons_dict = {key: try_float(val) for key, val in cons_dict.items()}
+        rate_dict = {key: try_float(val) for key, val in rate_dict.items()}
 
         self.cons_dict = cons_dict
 
@@ -35,21 +71,21 @@ class Bldg:
             'Steam': cons_dict['Steam'] * carbon_dict['Steam'],
         }
         self.energy_norm = {
-            'Elec': cons_dict['Elec'] * energy_dict['Elec'],
-            'Gas': cons_dict['Gas'] * energy_dict['Gas'],
-            'Steam': cons_dict['Steam'] * energy_dict['Steam'],
+            'Elec': cons_dict['Elec'] * kbtu_dict['Elec'],
+            'Gas': cons_dict['Gas'] * kbtu_dict['Gas'],
+            'Steam': cons_dict['Steam'] * kbtu_dict['Steam'],
         }
 
         self.eui = {
-            'Elec': (1000 * self.energy_norm['Elec']) / area,
-            'Gas': (1000 * self.energy_norm['Gas']) / area,
-            'Steam': (1000 * self.energy_norm['Steam']) / area,
+            'Elec': (self.energy_norm['Elec']) / area,
+            'Gas': (self.energy_norm['Gas']) / area,
+            'Steam': (self.energy_norm['Steam']) / area,
         }
 
         self.area = area
         self.bldg_type = bldg_type
         self.limit_dict = {key: val * area for key, val in limit_dict[bldg_type].items()}
-        self.fine = fineval
+        self.fine = fine_per_tCO2
         self.carbon_total = sum(self.carbon_dict.values())
 
         def fines(carbon_total, limit_dict, fine):
@@ -100,64 +136,158 @@ class Bldg:
         return summary
 
 
-
-
-
-
-
-
-def make_pie_charts(cost, carbon, eui):
-
-    # cost pie
-    cost_pie = go.Pie(values=cost['Value'],
-                      labels=cost['SubTable'],
-                      hole=0.5,
-                      text=carbon['SubTable'],
-                      marker={'colors': piecolors},
-                      showlegend=False
-                      )
-
-    layout = go.Layout(title='Annual Energy Cost by Fuel Type ($)',
-                       font={'family': 'Futura LT BT'},
-                       autosize=True,
-                       height=piedims,
-                       width=piedims
-                       )
-    cost_fig = go.Figure([cost_pie], layout)
-
-    # carbon pie
-    carbon_pie = go.Pie(values=carbon['Value'],
-                        labels=carbon['SubTable'],
-                        text=carbon['SubTable'],
+def make_pie_subplots(cost, carbon, eui):
+    cost_trace = go.Pie(values=cost['Value'],
+                        labels=cost['SubTable'],
                         hole=0.5,
+                        domain={'x': [0.0, 0.3],
+                                'y': [0, 1]},
+                        text=carbon['SubTable'],
                         marker={'colors': piecolors},
-                        showlegend=False
+                        showlegend=False,
                         )
+    carbon_trace = go.Pie(values=carbon['Value'],
+                          labels=carbon['SubTable'],
+                          text=carbon['SubTable'],
+                          hole=0.5,
+                          domain={'x': [0.33, 0.63],
+                                  'y': [0, 1]},
+                          marker={'colors': piecolors},
+                          showlegend=False,
+                          )
 
-    layout = go.Layout(title='Annual Carbon Emissions by Fuel Type (tCO2/yr)',
-                       font={'family': 'Futura LT BT'},
-        autosize=False,
-        height=piedims,
-        width=piedims
+    eui_trace = go.Pie(values=eui['Value'],
+                       labels=cost['SubTable'],
+                       text=carbon['SubTable'],
+                       hole=0.5,
+                       domain={'x': [0.66, 0.97],
+                               'y': [0, 1]},
+
+                       marker={'colors': piecolors},
+                       showlegend=False,
                        )
-    carbon_fig = go.Figure([carbon_pie], layout)
 
-    # eui
-    eui_pie = go.Pie(values=eui['Value'],
-                     labels=eui['SubTable'],
-                     text=carbon['SubTable'],
-                     hole=0.5,
-                     marker={'colors': piecolors},
-                     showlegend=False
-                     )
-    layout = go.Layout(title='Annual Energy Use Intensity by Fuel Type (kbtu/sf/yr)',
+    layout = go.Layout(
                        font={'family': 'Futura LT BT'},
-    autosize=False,
-    height=piedims,width=piedims)
-    eui_fig = go.Figure([eui_pie], layout)
+                       paper_bgcolor='rgba(0,0,0,0)',
+                       plot_bgcolor='rgba(0,0,0,0)',
+                       autosize=False,
+                       width=700,
+                       height=300,
 
-    return (cost_fig, eui_fig, carbon_fig)
+                       xaxis={'showgrid': False},
+                       yaxis={'showgrid': False},
+                       annotations=[
+                           {
+                               'x': 0.1,
+                               'y': 1.555,
+                               'xref': 'paper',
+                               'yref': 'paper',
+                               'text': 'Cost ($)',
+                               'showarrow': False,
+                               'font': {'size': 17, 'color': 'rgb(60, 60, 60)'},
+                               'align': 'left',
+                           },
+                           {
+                               'x': 0.47,
+                               'y': 1.555,
+                               'xref': 'paper',
+                               'yref': 'paper',
+                               'text': 'Carbon (tCO2)',
+                               'showarrow': False,
+                               'font': {'size': 17, 'color': 'rgb(60, 60, 60)'},
+                               'align': 'left',
+                           },
+                           {
+                               'x': 0.88,
+                               'y': 1.555,
+                               'xref': 'paper',
+                               'yref': 'paper',
+                               'text': 'EUI (kbtu/sf/yr)',
+                               'showarrow': False,
+                               'font': {'size': 17, 'color': 'rgb(60, 60, 60)'},
+                               'align': 'left',
+                           },
+                       ]
+                       )
 
+    # height=piedims,
+    # width=piedims
+
+    fig = go.Figure([cost_trace, carbon_trace, eui_trace], layout)
+
+    # fig = tools.make_subplots(1, 3, subplot_titles=['Annual Energy Use Intensity by Fuel Type (kbtu/sf/yr)',
+    #                                                 'Annual Energy Cost by Fuel Type ($)',
+    #                                                 'Annual Carbon Emissions by Fuel Type (tCO2/yr)',
+    #                                                 ])
+    # fig.add_trace(eui_trace, 1, 1)
+    # fig.add_trace(cost_trace, 1, 2)
+    # fig.add_trace(carbon_trace, 1, 3)
+    return fig
+
+
+#
+# def make_pie_charts(cost, carbon, eui):
+#
+#     # cost pie
+#     cost_pie = go.Pie(values=cost['Value'],
+#                       labels=cost['SubTable'],
+#                       hole=0.5,
+#                       text=carbon['SubTable'],
+#                       marker={'colors': piecolors},
+#                       showlegend=False
+#                       )
+#
+#     layout = go.Layout(title='Annual Energy Cost by Fuel Type ($)',
+#                        font={'family': 'Futura LT BT'},
+#                        autosize=False,
+#                        height=piedims,
+#                        width=piedims,
+#                        paper_bgcolor='rgba(0,0,0,0)',
+#                        plot_bgcolor='rgba(0,0,0,0)',
+#                        )
+#     cost_fig = go.Figure([cost_pie], layout)
+#
+#     # carbon pie
+#     carbon_pie = go.Pie(values=carbon['Value'],
+#                         labels=carbon['SubTable'],
+#                         text=carbon['SubTable'],
+#                         hole=0.5,
+#                         marker={'colors': piecolors},
+#                         showlegend=False
+#                         )
+#
+#     layout = go.Layout(title='Annual Carbon Emissions by Fuel Type (tCO2/yr)',
+#                        font={'family': 'Futura LT BT'},
+#                        paper_bgcolor='rgba(0,0,0,0)',
+#                        plot_bgcolor='rgba(0,0,0,0)',
+#         autosize=False,
+#         height=piedims,
+#         width=piedims
+#                        )
+#     carbon_fig = go.Figure([carbon_pie], layout)
+#
+#     # eui
+#     eui_pie = go.Pie(values=eui['Value'],
+#                      labels=eui['SubTable'],
+#                      text=carbon['SubTable'],
+#                      hole=0.5,
+#                      marker={'colors': piecolors},
+#                      showlegend=False,
+#
+#
+#                      )
+#     layout = go.Layout(title='Annual Energy Use Intensity by Fuel Type (kbtu/sf/yr)',
+#                        font={'family': 'Futura LT BT'},
+#                        paper_bgcolor='rgba(0,0,0,0)',
+#                        plot_bgcolor='rgba(0,0,0,0)',
+#                        autosize=False,
+#                        height=piedims,
+#                        width=piedims
+#                        )
+#
+#     eui_fig = go.Figure([eui_pie], layout)
+#     return (cost_fig, eui_fig, carbon_fig)
 
 
 def make_carbon_bullet(carbon, co2limit):
@@ -174,6 +304,7 @@ def make_carbon_bullet(carbon, co2limit):
     layout = go.Layout(barmode='overlay',
                        font={'family': 'Futura LT BT'},
                        legend={'x': 1.07},
+                       autosize=False,
                        width=400,
                        height=700,
                        xaxis={'showticklabels': False,
@@ -185,6 +316,8 @@ def make_carbon_bullet(carbon, co2limit):
                               'zeroline': False,
                               'title': "Metric Tons CO2",
                               'fixedrange': True},
+                       paper_bgcolor='rgba(0,0,0,0)',
+                       plot_bgcolor='rgba(0,0,0,0)',
 
                        )
 
@@ -289,20 +422,15 @@ def make_carbon_bullet(carbon, co2limit):
     return figure
 
 
-
-
 def make_cost_bar(fine, cost):
-
-
     data = []
-
 
     try:
         elec_costs = cost.iloc[0, :]['Value']
         elec_traces = go.Bar(x=fine.SubTable,
-                          y=[elec_costs] * len(fine.SubTable),
-                          name='Elec ($)',
-                          marker={'color': piecolors[0]})
+                             y=[elec_costs] * len(fine.SubTable),
+                             name='Elec ($)',
+                             marker={'color': piecolors[0]})
         data.append(elec_traces)
     except:
         pass
@@ -310,9 +438,9 @@ def make_cost_bar(fine, cost):
     try:
         gas_costs = cost.iloc[1, :]['Value']
         gas_traces = go.Bar(x=fine.SubTable,
-                             y=[gas_costs] * len(fine.SubTable),
-                             name='Gas ($)',
-                             marker={'color': piecolors[1]})
+                            y=[gas_costs] * len(fine.SubTable),
+                            name='Gas ($)',
+                            marker={'color': piecolors[1]})
         data.append(gas_traces)
     except:
         pass
@@ -320,30 +448,34 @@ def make_cost_bar(fine, cost):
     try:
         steam_costs = cost.iloc[2, :]['Value']
         steam_traces = go.Bar(x=fine.SubTable,
-                                 y=[steam_costs] * len(fine.SubTable),
-                                 name='Steam ($)',
-                                 marker={'color': piecolors[2]})
+                              y=[steam_costs] * len(fine.SubTable),
+                              name='Steam ($)',
+                              marker={'color': piecolors[2]})
         data.append(steam_traces)
     except:
         pass
 
     fine_traces = go.Bar(x=fine.SubTable,
-                          y=fine.Value,
-                          name='Carbon Fine ($)',
-                          marker={'color': 'rgb(200, 83, 94)'}
-                          )
+                         y=fine.Value,
+                         name='Carbon Fine ($)',
+                         marker={'color': 'rgb(200, 83, 94)'}
+                         )
 
     data.append(fine_traces)
 
     layout = go.Layout(barmode='stack',
+                       autosize=False,
                        width=700,
-                       height=500,
+                       height=400,
                        title='Annual Building Energy Cost Over Time',
                        font={'family': 'Futura LT BT'},
                        legend={'orientation': 'h',
                                'x': 0.2,
                                'y': -0.1},
                        xaxis={'fixedrange': True},
-                       yaxis={'fixedrange': True})
+                       yaxis={'fixedrange': True},
+                       paper_bgcolor='rgba(0,0,0,0)',
+                       plot_bgcolor='rgba(0,0,0,0)',
+                       )
     fig = go.Figure(data, layout)
     return fig
