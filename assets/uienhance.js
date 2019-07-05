266f0323f403eb15ee8d353b202fd8c85577ee33
michaelsweeney <itsN0T4me!>
2019-07-05 08:29:53 -0400

    healthcare

---
+++ assets/uienhance.js
@@ -0,0 +1,17 @@
+// console.log('loaded')
+
+
+
+
+
+// var slice = document.getElementsByClassName('slice')
+// console.log(slice)
+
+
+
+
+
+// slice.forEach((item) => {
+// 	console.log(item)
+
+// })
